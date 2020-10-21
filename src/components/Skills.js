6265d0b5b5d7d36734fcdb0434b246eae8c2e93d
Brandon Nolan <brandonnolan3123@gmail.com>
2020-10-20 19:28:50 -0600

    Adding skills.

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,11 +3,14 @@ import Grid from "@material-ui/core/Grid";
 import Avatar from "@material-ui/core/Avatar";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
-import CardHeader from "@material-ui/core/CardHeader";
-import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import Divider from "@material-ui/core/Divider";
+import java from "../images/java.png"
+import spring from "../images/spring.png"
+import springSecurity from "../images/springSecurity.png"
+import sql from "../images/sql.png"
+import html from "../images/html-5.png"
 
 class Skills extends React.Component {
     render() {
@@ -16,15 +19,34 @@ class Skills extends React.Component {
                 Skills
             </Typography>
             <Divider className={'centerDivider'} />
-            <Grid spacing={5} direction={"column"} container>
+            <Grid spacing={5} className={'skillsGrid'} direction={"column"} container>
                 <Grid item>
-                    <Card>
-                        <CardContent>
-                            <Typography variant={"h5"} className={'headerFont'}>
-                                Languages/Frameworks
-                            </Typography>
-                        </CardContent>
-                    </Card>
+                    <Typography variant={"h5"} className={'headerFont'}>
+                        Languages/Frameworks
+                    </Typography>
+                    <br/>
+                    <Grid container>
+                        <Grid item className={'skillGridItem'}>
+                            <Avatar src={springSecurity} className={'skillsIcon'} variant={"square"}></Avatar>
+                            <Typography variant={"h6"} className={'informationFont'}>Spring <br/>Security</Typography>
+                        </Grid>
+                        <Grid item className={'skillGridItem'}>
+                            <Avatar src={java} className={'skillsIcon'} variant={"square"}></Avatar>
+                            <Typography variant={"h6"} className={'informationFont'}>Java</Typography>
+                        </Grid>
+                        <Grid item className={'skillGridItem'}>
+                            <Avatar src={spring} className={'skillsIcon'} variant={"square"}></Avatar>
+                            <Typography variant={"h6"} className={'informationFont'}>Spring</Typography>
+                        </Grid>
+                        <Grid item className={'skillGridItem'}>
+                            <Avatar src={sql} className={'skillsIcon'} variant={"square"}></Avatar>
+                            <Typography variant={"h6"} className={'informationFont'}>SQL</Typography>
+                        </Grid>
+                        <Grid item className={'skillGridItem'}>
+                            <Avatar src={html} className={'skillsIcon'} variant={"square"}></Avatar>
+                            <Typography variant={"h6"} className={'informationFont'}>HTML</Typography>
+                        </Grid>
+                    </Grid>
                 </Grid>
 
                 <Grid item>
@@ -61,7 +83,7 @@ class Skills extends React.Component {
                     <Card>
                         <CardContent>
                             <Typography variant={"h5"} className={'headerFont'}>
-                                IDE's
+                                IDE's/Tools
                             </Typography>
                         </CardContent>
                     </Card>
